fb9951710a5f8c415f130740c901dd537f4948a2
Helios <hien.dnnt@gmail.com>
2020-05-26 21:10:27 +0700

    fix Home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,22 +1,45 @@
 import React, { Component } from 'react';
 
+import axios from 'axios';
+
 import './css/HomePage.css';
 import Card from "../components/Card";
 import Header from "../components/Header"
+import PostStore from "../stores/PostStore";
 
 export default class extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      listPost: PostStore.posts
+    }
+  }
+  
+  async componentDidMount() {
+    try {
+      const res = await axios.get("https://hilarious-pointed-taker.glitch.me/api/posts/get");
+      const postData = res.data;
+      PostStore.posts = postData;
+      this.setState({
+        listPost: postData
+      });
+    } catch(e) {
+      console.log(e);
+    }
+  }
   render() {
     return (
       <div className="home-page">
         <Header />
         <div className="content" >
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {
+            this.state.listPost.map(item => {
+              return (
+                <Card post={item} />
+              );
+            })
+          }
         </div>
       </div>
     );
